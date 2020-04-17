4ad69393fd22e959aa4adaffc11527af79d3f2ed
Marlon <marmoneymar@icloud.com>
2020-04-17 14:04:36 -0400

    Trying to work through it still

--- adv.py
+++ adv.py
@@ -55,14 +55,45 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = ['n', 'n', 's', 's', 'w', 'w', 'e', 'e', 'e', 'e', 'w', 'w', 's', 's']
 
+def bfs(starting_room):
+    queue = Queue()
+    queue.enqueue([starting_room.id])
+    pass
 
+
+def dft(room, visited=None):
+    stack = Stack()
+    stack.push(room.id)
+    
+    if visited is None:
+        visited = {}
+        # Base case: How do we know we're done?
+        # We're done when the room has no exits
+        
+        # Track visited nodes
+        visited[room.id] = {}
+        
+        # Call the function recursively - on rooms not visited
+        for exit in room.get_exits():
+            if exit not in visited:
+                dft(exit, visited)
+
+
+
+
+
+# current_room
+prev_room = None
+direction = None
+reverse_direction = None
 
 def traverse_map(world, current_room):
-    # print("Player's current room: ", player.current_room)
-    # player.travel('n')
-    # print("Player's new current room: ", player.current_room)
-    queue = Queue()
-    queue.enqueue([current_room.id])
+    
+    
+    
+    while True:
+        directions = player.get_exits()
+        player.travel(directions[0])
     
     visited = {}
     path_of_room_ids = queue.dequeue()
@@ -70,13 +101,81 @@ def traverse_map(world, current_room):
     print('Path: ', path_of_room_ids)
     print("Node: ", room_id)
     
-    while queue.size() > 0:
-        if room_id not in visited:
-            visited[room_id] = current_room.get_exits()
+oposite_cardinals = { 'n': 's', 'w': 'e', 's': 'n', 'e': 'w' }
+
+# PSEUDO CODE
+# init
+# get current room
+# - room 0
+# get the exits o fthe current room
+# [n, s, w, e]
+
+current_room = player.current_room
+exits = player.current_room.get_exits()
+
+# add current room init to visited
+# should look like this -> {0: {n: ?, s: ?, w: ?, e: ?} }
+
+def set_initial_cardinal_directions(room):
+    exits = room.get_exits()
+    cardinal_directions = {}
+    for exit in exits:
+        cardinal_directions[exit] = '?'
+    return cardinal_directions
+
+visited = {}
+
+visited[current_room.id] = set_initial_cardinal_directions(current_room)
+
+print(visited[current_room.id])
+
+# initialize a stack
+# -> 0 (room id) OR... <- This one probably won't work
+# -> (direction = None, prev_room = None)
+# add room 0 to the stack via Push
+
+stack = Stack()
+stack.push(direction=None, prev_room=None)
+
+# start traversal in DFT mode
+# room_info = pop -> (direction, previous)
+room_info = stack.pop()
+# current_room = current_room.id
+# previous_room = room_info[1]
+# direction = room_info[0]
+# get the current room exits from visited
+
+# check if current room isn't visited
+# if not, then add to visited
+# add to visited
+# should look like this -> { current_room: {exits...?} }
+
+# This should fail onthe first iteration because there is no previous room **
+# if previous room is not None:
+# this is where we update our previous room
+# visited[previous_room][direction] = current_room
 
 
+# This should fail on the first iteration because there is no direction **
+# update current_room exits if we have a direction
+# if direction is not None:
+# visited[current_room][reverse_direction] = previous_room
 
-traverse_map(world, player.current_room)
+# Loop over unvisited exits / or maybe all exits
+    # move in that direction
+    # Update traversal_path -> direction
+    # update the stack -> (direction, current_room)
+    
+# if there are no exits that are unvisited
+# enter into bft mode -> This will probably be a helper function
+
+
+# bft will traverse over our visited graph
+    # the destination is a room with a question mark
+    # building a path to traverse after finding the destination <~~ ?
+
+
+# traverse_map(world, player.current_room) ***
 
 
 
